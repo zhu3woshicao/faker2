30d39cdf1213a203069b09f7d3b2c0756683e8b5
zhu3woshicao <english736218369@qq.com>
2021-11-12 15:06:41 +0800

    fix:修复cfdl 助力url

--- gua_wealth_island_help.js
+++ gua_wealth_island_help.js
@@ -487,7 +487,7 @@ function getUrlQueryParams(url_string, param) {
 function getAuthorShareCode(url) {
   return new Promise(async resolve => {
     const options = {
-      "url": `${url}?${new Date()}`,
+      "url": `${url}`,
       "timeout": 10000,
       "headers": {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
