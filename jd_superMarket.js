bfbdf0c272da16c811661ef7a69b3311b3d0b90e
zhu3woshicao <english736218369@qq.com>
2021-11-15 14:17:47 +0800

    update:删除助力

--- jd_superMarket.js
+++ jd_superMarket.js
@@ -1637,8 +1637,9 @@ function jsonParse(str) {
 }
 //==========================以下是给作者助力 免费拿,省钱大赢家等活动======================
 async function helpAuthor() {
-  await barGain();//免费拿
-  await bigWinner();//省钱大赢家
+    // todo... 助力
+  // await barGain();//免费拿
+  // await bigWinner();//省钱大赢家
 }
 async function barGain() {
   let res = await getAuthorShareCode2('https://raw.githubusercontent.com/Aaron-lv/updateTeam/master/shareCodes/jd_barGain.json')
